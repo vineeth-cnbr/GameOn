7a448a7dafa4ca8c929d42f9492d2c5d2223d02f
shreyansh <shreyanshrocknroll@gmail.com>
2017-09-15 17:36:33 +0530

    user parsing

--- Server.js
+++ Server.js
@@ -1,8 +1,68 @@
 var express = require("express"),
     app = express(),
+    bodyParser = require('body-parser'),
     path = __dirname + '/views/',
     router = express.Router(),
-    MongoClient = require('mongodb').MongoClient;
+    mongoose = require('mongoose'),
+    bcrypt = require('bcrypt'),
+    SALT_WORK_FACTOR = 10;
+
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ extended: false }));   // to support URL-encoded bodie
+
+mongoose.connect('mongodb://localhost:27017/gameon', {useMongoClient: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
+
+var Schema = mongoose.Schema;
+    
+
+var UserSchema = new Schema({
+    username: { type: String, required: true, index: { unique: true } },
+    password: { type: String, required: true }
+});
+
+
+
+UserSchema.pre('save', function(next){ 
+var user = this;
+// only hash the password if it has been modified (or is new)
+if (!user.isModified('password')) return next();
+
+// generate a salt
+bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+    if (err) return next(err);
+
+    // hash the password along with our new salt
+    bcrypt.hash(user.password, salt, function(err, hash) {
+        if (err) return next(err);
+
+        // override the cleartext password with the hashed one
+        user.password = hash;
+        next();
+    });
+});
+
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+}
+        
+
+
+var UserKitty = mongoose.model('UserKitty',UserSchema);
+var user1 = new UserKitty( {
+    username: "nigga",
+    password: "chootiya"
+});
+console.log(user1);
 
 
 // addictional code
@@ -22,15 +82,21 @@ router.use(function (req,res,next) {
   next();
 });
 
-MongoClient.connect('mongodb://localhost:27017/GameOn', function(err, db) {
 
-  router.get("/",function(req,res){
-    res.render('login');
-    //res.sendFile(path + "index.html");
-  });
-
-  app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
-  });
-
+router.get("/",function(req,res){
+res.render('login');
+//res.sendFile(path + "index.html");
 });
+
+app.post('/register',function(req, res) {
+    var currentUser = new UserKitty( {
+        username:    req.body.username,
+        password:   req.body.password
+    });
+    console.log(currentUser);
+});
+
+app.listen(app.get('port'), function() {
+console.log('Node app is running on port', app.get('port'));
+});
+
